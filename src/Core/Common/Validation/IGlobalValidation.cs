c4fc074cdf47cc8b0c7f95b2477a18bae8b0f170
duong.vanvu <duong.vuvan@alta.com.vn>
2022-03-22 17:53:12 +0700

    global service

--- src/Core/Common/Validation/IGlobalValidation.cs
+++ src/Core/Common/Validation/IGlobalValidation.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using Core.Bases;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Core.Common.Validation
 {
-    public interface IGlobalValidation
+    public interface IGlobalValidation<TEntity, Tid> where TEntity : class
     {
+        bool IsExistId(Tid id, Expression<Action<TEntity>> preticate);
+        bool IsValidFileName(string fileName);
     }
 }
